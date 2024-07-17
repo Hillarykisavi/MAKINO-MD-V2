7e8f9b63323a1a239a8b04f749f57a43606e0e2e
Hillarykisavi <165393757+Hillarykisavi@users.noreply.github.com>
2024-07-17 22:53:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -12,23 +12,23 @@ global.antitags = false;
 
 //auto functioner
 global.autoTyping = false;                //make true to enable auto typing
-global.autoRecord = true;                //make true to enable auto recording
+global.autoRecord = false;                //make true to enable auto recording
 global.groupevent = true;                //This is the new variable for controlling group event handling.
-global.statusseen = false;                 //make true to view statuses 
+global.statusseen = true;                 //make true to view statuses 
 
 
-global.Owner = [""];
-global.OwnerNumber = [""];
+global.Owner = ["Hillary"];
+global.OwnerNumber = ["+254742657861"];
 global.ownertag = [""]; 
-global.OwnerName =  "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";
-global.BotName = "🐦Makino-md-v2";
-global.packname = "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";                             //Do not change.
-global.author = "🐦Makino md ᴍᴜʟᴛɪ-ᴅᴇᴠɪᴄᴇ";                               //Do not change.
+global.OwnerName =  "Hillary";
+global.BotName = "🐦i.am_Hillary-md-v2";
+global.packname = "Hillary";                             //Do not change.
+global.author = "Hillary";                               //Do not change.
 global.BotSourceCode = "https://github.com/anonphoenix007/MAKINO-MD-V2"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/KEg0G3UUs1G39ikdyfF5Pm "; 
 global.SupportGroupLink = "https://chat.whatsapp.com/KEg0G3UUs1G39ikdyfF5Pm"; //Do not change!
 //global.dbase = process.env.DBASE || "";//Mongodb here
-global.AuthorWhatsapp = "https://wa.me/2347080968564"; //Bot creator WhatsApp link,don't change or bot will not work.
+global.AuthorWhatsapp = "https://wa.me/254742657861"; //Bot creator WhatsApp link,don't change or bot will not work.
 
 global.sessID = process.env.sessID || ``
 global.prefa = ["/"]
@@ -67,11 +67,11 @@ global.mess = {
     useradmin: 'Only group Admin can use the command 😂 ',
     botadmin: 'Make me Admin first 😌📍.',
     botowner: 'Only my *Owner* can use this command,Dont trespass, Baka!',
-    grouponly: 'This command is only made for *Groups*, Baka!',
-    privateonly: 'This command is only made for *Private Chat*, Baka!',
+    grouponly: 'This command is only made for *Groups*, idiot!',
+    privateonly: 'This command is only made for *Private Chat*, idiot!',
     botonly: 'Only the *Bot itself* can use this command!',
     waiting: 'Wait a lil bit (¬_¬)ﾉ...',
-    nolink: ' provide me *link*, Baka!',
+    nolink: ' provide me *link*, idiot!',
     error: 'An error occurd!',
     banned: 'You cant use the commands because you Are *Banned*',
     bangc: 'This Group is *Banned* from using Commands!',
